9c1b865fc827845cf7af9b91b72fe369cd6ab976
Thomas Spycher <tspycher@zerodine.com>
2013-06-13 15:01:08 +0200

    Added postgres config file

--- modules/postgres/manifests/install.pp
+++ modules/postgres/manifests/install.pp
@@ -7,7 +7,16 @@ class postgres::install {
         }
 	exec { "initdb":
 		command => "/sbin/service postgresql initdb",
-		creates => "/var/lib/pgsql/data",		
+		creates => "/var/lib/pgsql/data",
+		require => Package['postgresql-server'],
+	}
+	file { "/var/lib/pgsql/data/postgresql.conf":
+		ensure => "present",
+		source => "puppet:///modules/postgres/var/lib/pgsql/data/postgresql.conf",
+		require => Exec['initdb'],
+		owner => "postgres",
+		group => "postgres",
+		mode => 600,
 	}
         service { "postgresql":
                 enable => true,
